e5f97c7fe7204d58a99a282fe8cc8a96d7407759
Michael <veg.it.up@live.com>
2019-03-11 15:47:36 -0700

    create authform and homepage components, styles

---
+++ src/components/AuthForm.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+export default class AuthForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      username: "",
+      password: "",
+      profileUrl: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  render() {
+    const { email, username, password, profileUrl } = this.state;
+    const { heading, buttonText } = this.props;
+    return (
+      <div>
+        <div className="row justify-content-md-center text-center">
+          <div className="col-md-6">
+            <form onSubmit={this.handleSubmit}>
+              <h2>{heading}</h2>
+              <label htmlFor="email">Email:</label>
+              <input
+                className="form-control"
+                id="email"
+                name="email"
+                onChange={this.handleChange}
+                value={email}
+                type="text"
+              />
+              <label htmlFor="password">Password:</label>
+              <input
+                className="form-control"
+                id="password"
+                name="password"
+                onChange={this.handleChange}
+                type="password"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
